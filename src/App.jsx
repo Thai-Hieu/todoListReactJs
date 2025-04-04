91db052ab144648ac17bd7da7cfd3754bb760fde
Thai-Hieu <thaihieu20062006@gmail.com>
2025-04-04 19:59:46 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoApp from "./components/TodoList";
+
+function App() {
+    return (
+        <div>
+            <TodoApp />
+        </div>
+    );
+}
+
+export default App;
